9005dbc137c43d754d45e38f8add44d3bb8f3664
LakhalEngineering <125914042+LakhalEngineering@users.noreply.github.com>
2023-10-24 14:19:25 +0200

    Create main.c
    two file in c show how memory allocation can effect the performance of the program

---
+++ bycol.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+#include <stdbool.h>
+#include <stdlib.h>
+#include <stdint.h>
+
+#define ROWS 100000
+#define COLS 10000
+
+
+int twodarray[ROWS][COLS]; // 4GB
+
+int main(){
+    for (int i=0; i < ROWS; i++){
+        for(int j=0; j < COLS; j++){
+            twodarray[i][j] = rand();
+        }
+    }
+
+    int64_t sum = 0;
+    for (int j=0; j < COLS; j++){
+        for(int i=0; i < ROWS; i++){
+            sum += twodarray[i][j];
+        }
+    }
+    printf("sum = %lld\n", sum);
+}
+
+    
